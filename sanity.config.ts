9f49d03ec4006c4b4354a159b4d70dce04a695c1
Alessio <carninoalessio@gmail.com>
2026-06-26 12:15:08 +0200

    Remove visionTool to fix build

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { visionTool } from "@sanity/vision";
 import project from "./schemas/project";
 
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
   title: "Vela Studio",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
   schema: {
     types: [project],
   },
